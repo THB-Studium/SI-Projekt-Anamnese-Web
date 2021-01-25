b0792c943eed82850426b2f36451ddf23595cdc9
MistraForest <mistraf16@yahoo.fr>
2021-01-25 03:36:33 +0100

    Test new git projekt2

--- src/environments/environment.aws.ts
+++ src/environments/environment.aws.ts
@@ -7,6 +7,6 @@ export const environment = {
   currentEnvironment: 'AWS',
 
   // Paths:
-  baseUrl: 'http://ec2-34-207-186-190.compute-1.amazonaws.com:8080',
+  baseUrl: 'http://ec2-34-207-186-190.compute-1.amazonaws.com:8080'
 
 }

162f3dcea4829867de084aaf8b3a47ab85f28da0
Steve Carmel <egoue2001@gmail.com>
2024-04-24 04:37:56 +0200

    chore: migration to standalone - step 3

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,49 @@
-import { enableProdMode } from '@angular/core'
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
+import { enableProdMode, importProvidersFrom } from '@angular/core'
 
-import { AppModule } from './app/app.module'
+
 import { environment } from './environments/environment'
+import { AppComponent } from './app/app.component'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatCardModule } from '@angular/material/card'
+import { provideAnimations } from '@angular/platform-browser/animations'
+import { routes } from './app/app.route'
+import { provideRouter } from '@angular/router'
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser'
+import { CommonModule } from '@angular/common'
+import { AuthGuard } from './app/core/authentification-and-authority/auth.guard'
+import { AppConfigService } from './app/core/app-config.service'
+import { LoginService } from './app/components/log-in/login.service'
+import { AuthInterceptor } from './app/core/authentification-and-authority/auth.Interceptor'
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatNativeDateModule } from '@angular/material/core'
 
 if (environment.production) {
   enableProdMode()
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(
+      CommonModule, BrowserModule, MatCardModule, MatFormFieldModule, FormsModule, MatInputModule, MatIconModule,
+      MatButtonModule, MatSnackBarModule, MatDialogModule, MatNativeDateModule
+    ),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    LoginService,
+    AppConfigService,
+    AuthGuard,
+    provideRouter(routes),
+    provideAnimations()
+  ]
+})
   .catch(err => console.error(err))

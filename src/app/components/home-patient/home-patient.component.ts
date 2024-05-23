206a18cee2d5b61565bb743e5d8489565f141584
Steve Ngalamo <egoue2001@gmail.com>
2024-05-23 02:11:52 +0200

    refac: refactoring in home-patient.component.ts|html|css

--- src/app/components/home-patient/home-patient.component.ts
+++ src/app/components/home-patient/home-patient.component.ts
@@ -1,33 +1,41 @@
-import { Component, OnInit } from '@angular/core'
-import { rootingPaths } from '../../shared/const/rooting-paths'
+import {Component, OnInit} from '@angular/core'
+import {rootingPaths} from '../../shared/const/rooting-paths'
 
-import { SessionService } from '../../core/authentification-and-authority/session.service'
-import { IPerson } from '../../model/person.interface'
-import { MyProfileModalComponent } from './my-profile-modal/my-profile-modal.component'
-import { Router, RouterLink } from '@angular/router'
-import { IAddress } from '../../model/address.interface'
-import { PersonService } from '../../services/person.service'
-import { environment } from '../../../environments/environment'
-import { MatDialog, MatDialogRef } from '@angular/material/dialog'
-import { MatSnackBar } from '@angular/material/snack-bar'
-import { MatIconModule } from '@angular/material/icon';
-import { NgIf, NgOptimizedImage } from '@angular/common';
-import { HeaderComponent } from '../../shared/components/header/header.component';
+import {SessionService} from '../../core/authentification-and-authority/session.service'
+import {IPerson} from '../../model/person.interface'
+import {MyProfileModalComponent} from './my-profile-modal/my-profile-modal.component'
+import {Router, RouterLink} from '@angular/router'
+import {IAddress} from '../../model/address.interface'
+import {PersonService} from '../../services/person.service'
+import {MatDialog} from '@angular/material/dialog'
+import {MatSnackBar} from '@angular/material/snack-bar'
+import {MatIconModule} from '@angular/material/icon';
+import {AsyncPipe, NgIf, NgOptimizedImage} from '@angular/common';
+import {HeaderComponent} from '../../shared/components/header/header.component';
+import {HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable, throwError} from "rxjs";
+import {map} from "rxjs/operators";
+import {BackgroundComponent} from "../background/background.component";
+
+const importedComponents = [
+  HeaderComponent, BackgroundComponent
+]
 
 @Component({
-    selector: 'app-home-patient',
-    templateUrl: './home-patient.component.html',
-    styleUrls: ['./home-patient.component.css'],
-    standalone: true,
-    imports: [HeaderComponent, NgIf, RouterLink, NgOptimizedImage, MatIconModule]
+  selector: 'app-home-patient',
+  templateUrl: './home-patient.component.html',
+  styleUrls: ['./home-patient.component.css'],
+  standalone: true,
+  imports: [
+    importedComponents, NgIf, RouterLink, NgOptimizedImage, MatIconModule, AsyncPipe
+  ]
 })
 export class HomePatientComponent implements OnInit {
-  headerTitle: string = 'Home - Willkomen!'
-  currentUser: IPerson = <IPerson>{}
-
+  readonly headerTitle: string = 'Home - Willkomen!'
   readonly aufnahme_path: string
   readonly patient_info_view_path: string
-  readonly currentEnvironment: string
+
+  currentUser$: Observable<IPerson> = undefined
 
   constructor(
     private router: Router,
@@ -38,7 +46,6 @@ export class HomePatientComponent implements OnInit {
   ) {
     this.aufnahme_path = '/' + rootingPaths.aufnahme
     this.patient_info_view_path = '/' + rootingPaths.patient_info_view
-    this.currentEnvironment = environment.currentEnvironment
   }
 
   ngOnInit(): void {
@@ -46,14 +53,23 @@ export class HomePatientComponent implements OnInit {
   }
 
   openMyProfilModal(): void {
-    this.currentUser.address = <IAddress>{}
-    const dialogRef: MatDialogRef<MyProfileModalComponent> = this.dialog.open(MyProfileModalComponent, {
-      width: '750px',
-      data: {person: this.currentUser, personType: 'patient'}
-    })
-
-    dialogRef.afterClosed().subscribe(result => {
-    })
+    this.currentUser$.subscribe({
+        next: (currentUser: IPerson): void => {
+          currentUser.address = currentUser.address || <IAddress>{}
+          this.dialog.open(MyProfileModalComponent, {
+            width: '750px',
+            data: {person: currentUser, personType: 'patient'}
+          });
+        },
+        error: (error: HttpErrorResponse) => {
+          console.error('Error in HomePatientComponent.openMyProfilModal()', error);
+          this.snackBar.open('Could not open the Profile Modal', 'Close', {
+            duration: 4000
+          });
+          return throwError(() => new Error(error.message))
+        }
+      }
+    );
   }
 
   navTo(tabName: string): void {
@@ -61,18 +77,19 @@ export class HomePatientComponent implements OnInit {
   }
 
   private getCurrentUser(): void {
-    this.personService.getOne(this.sessionService.getUserId()).subscribe(
-      (person: IPerson): void => {
-        this.currentUser = person
-      }
-      ,
-      (err: Error): void => {
-        console.log('Error in HomePatientComponent.getCurrentUser()')
-        console.log(err)
-        this.snackBar.open('Could not fetch this current user data', 'Close', {
-          duration: 4000
+    this.currentUser$ = this.personService.getOne(this.sessionService.getUserId())
+      .pipe(
+        map((person: IPerson): IPerson => {
+          return person
+        }),
+        catchError((error: HttpErrorResponse) => {
+          console.error('Error in HomePatientComponent.testGetCurrentUser()', error);
+          this.snackBar.open('Could not fetch this current user data', 'Close', {
+            duration: 4000
+          });
+          return throwError(() => new Error(error.message))
         })
-      }
-    )
+      )
   }
+
 }
